534d8df1114edeeb2428c4c4f8bceec9f7038d71
unknown <huzaifa2680shaikh@gmail.com>
2025-02-01 13:11:06 +0530

    previous recipts displaying and class filter added

--- classvue/src/components/Home.vue
+++ classvue/src/components/Home.vue
@@ -1,64 +1,98 @@
 <template>
-<HeaderC></HeaderC>
-<div class="flex w-full shadow-lg rounded-lg bg-clip-border mt-4 container mx-auto">
+  <HeaderC></HeaderC>
+  <div class="flex mt-4 container mx-auto gap-2">
+    <label for="classFilter">Filter by Class:</label>
+    <select v-model="selectedClass" class="rounded-md border-2 border-slate-800">
+      <option value="All">All</option>
+      <option v-for="std in uniqueClasses" :key="std" :value="std">{{ std }}</option>
+    </select>
+  </div>
+  <div class="flex w-full shadow-lg rounded-lg bg-clip-border mt-4 container mx-auto">
     <table class="w-full table-auto">
-        <tr class="border-b border-slate-300 bg-rose-600 text-white">
-            <th class="text-xl p-2" colspan="8">Students</th>
-        </tr>
-        <tr class="border-b border-slate-300 hover:bg-slate-100">
-            <th class="p-2">S.N</th>
-            <th class="p-2">Name</th>
-            <th class="p-2">Std</th>
-            <th class="p-2">Admission Date</th>
-            <th class="p-2">Total Fees</th>
-            <th class="p-2">Paid</th>
-            <th class="p-2">Outstanding</th>
-            <th class="p-2">Receipt</th>
-        </tr>
+      <tr class="border-b border-slate-300 bg-rose-600 text-white">
+        <th class="text-xl p-2" colspan="8">Students</th>
+      </tr>
+      <tr class="border-b border-slate-300 hover:bg-slate-100">
+        <th class="p-2">S.N</th>
+        <th class="p-2">Name</th>
+        <th class="p-2">Std</th>
+        <th class="p-2">Admission Date</th>
+        <th class="p-2">Total Fees</th>
+        <th class="p-2">Paid</th>
+        <th class="p-2">Outstanding</th>
+        <th class="p-2">Receipt</th>
+      </tr>
 
-        <tr v-for="(i, index) in students" :key="i" class="border-b border-slate-300 hover:bg-slate-100 text-center">
-            <td class="p-2">{{ index + 1 }}</td>
-            <td class="p-2">{{ i.name }}</td>
-            <td class="p-2">{{ i.std }}</td>
-            <td class="p-2">{{ i.admission_date }}</td>
-            <td class="p-2">{{ i.total_fees }}</td>
-            <td class="p-2">{{ i.paid_fees }}</td>
-            <td class="p-2">{{ i.outstanding_fees }}</td>
-          <td class="p-2">
-            <router-link :to="'/receipt/'+i.id">
-              <button type="button" class="px-3 bg-sky-400 rounded-3xl text-white">Receipt</button>
-            </router-link>
-            <router-link :to="'/showreceipts/'+i.id">
-              <button type="button" class="px-3 bg-sky-400 rounded-3xl text-white">Show Receipts</button>
-            </router-link>
-          </td>
-        </tr>
+      <tr v-for="(i, index) in filteredStudents" :key="i"
+          class="border-b border-slate-300 hover:bg-slate-100 text-center">
+        <td class="p-2">{{ index + 1 }}</td>
+        <td class="p-2">{{ i.name }}</td>
+        <td class="p-2">{{ i.std }}</td>
+        <td class="p-2">{{ i.admission_date }}</td>
+        <td class="p-2">{{ i.total_fees }}</td>
+        <td class="p-2">{{ i.paid_fees }}</td>
+        <td class="p-2">{{ i.outstanding_fees }}</td>
+        <td class="p-2">
+          <router-link :to="'/receipt/'+i.id">
+            <button type="button" class="px-3 bg-sky-400 rounded-3xl text-white">Receipt</button>
+          </router-link>
+          <router-link :to="'/showreceipts/'+i.id">
+            <button type="button" class="px-3 bg-sky-400 rounded-3xl text-white">Show Receipts</button>
+          </router-link>
+        </td>
+      </tr>
     </table>
-</div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
 import HeaderC from './Header.vue';
+
 export default {
-    name: 'HomePage',
-    components:{
-        HeaderC
-    },
-    data() {
-        return {
-            students: [],
-        }
-    },
-    methods:{
-        async fetchData(){
-            let result = await axios.get('Students/')
-            this.students = result.data
-            console.log(result.data)
-        }
-    },
-    mounted(){
-        this.fetchData()
+  name: 'HomePage',
+  components: {
+    HeaderC
+  },
+  data() {
+    return {
+      students: [],
+      selectedClass: "All",  // Default selected option
+      selectedName: "",  // Filter by name (empty means no filter)
+      selectedOutstandingFees: null,  // Filter by outstanding fees (empty means no filter)
     }
+  },
+  computed: {
+    uniqueClasses() {
+      const classes = new Set(this.students.map(student => student.std));
+      /* 'this.students.map(student => student.std' with map() we can make array from a object
+      like here we have remove only std values from object using map method
+      now we want only unique values so for that reason this array is converted into set to
+      store unique values and to create Set we need to initialize it by instance like here we
+      have done 'const classes = new' classes is the instance for Set()   */
+
+      return [...classes].sort(); // Convert to array & sort
+      /* [...classes] converts Set object into array and than we sort it */
+
+      /* first we create a array of all std's available han passed/convert it to Set to fetch
+      unique values and than converted that Set into array and sort it */
+    },
+    // Filter students based on the selected class
+    filteredStudents() {
+      if (this.selectedClass === "All") {
+        return this.students;
+      }
+      return this.students.filter(student => student.std === this.selectedClass);
+    }
+  },
+  methods: {
+    async fetchData() {
+      let result = await axios.get('Students/')
+      this.students = result.data
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
 }
 </script>
--- classvue/src/components/ShowReceipts.vue
+++ classvue/src/components/ShowReceipts.vue
@@ -1,5 +1,80 @@
 <template>
-  <h2>Sun</h2>
+  <div v-for="i in receipt_data" :key="i">
+    <div class="flex flex-col items-center justify-center mt-4 min-h-screen bg-gray-50">
+      <div ref="receiptContent" id="receiptContent" class="bg-white shadow-md rounded-md mt-3 p-6 w-11/12 sm:w-6/12">
+        <div class="py-5  border-2  border-blue-500 ">
+          <label class="text-xl italic ml-2">SA</label>
+        </div>
+        <div class="py-3 flex border-2 border-t-0 border-blue-500 ">
+          <label class="text-lg ml-2">
+            First Floor, Chawre Manzil, Opposite Z.B. College, Sopara Gaon(West) 401203
+          </label>
+        </div>
+        <div class="py-5  border-2 border-t-0 border-blue-500 text-center">
+          <label class="text-2xl font-bold text-blue-950">Cash Receipt</label>
+          <div class="text-end mr-5">
+            <label class="text-base text-black font-normal">Date: </label>
+          </div>
+          <div class="text-start ml-2 mt-4">
+            <label class="text-lg">Student Name: </label>
+            <input class="text-lg w-1/2 border-0 border-b border-blue-500 outline-none"
+                    :value="i.stud_name"/>
+          </div>
+          <div class="text-start ml-2 mt-4">
+            <label class="text-lg">Rs: </label>
+            <input class="text-lg w-6/12 border-0 border-b border-blue-500 outline-none" :value="i.rs" />
+            <label class="text-lg">Std: </label>
+            <input class="text-lg w-3/12 border-0 border-b border-blue-500 outline-none" :value="i.std"/>
+          </div>
+          <div class="ml-2 mt-4">
+            <table class="w-7/12 sm:w-4/12">
+              <tr class="border-2 border-blue-500">
+                <th class="p-2 border-r border-blue-500">
+                  <label class="text-base text-black font-normal border-b-0">Total Amount Due</label>
+                </th>
+                <th class="p-2">
+                  <label class="text-base text-black font-normal">{{ i.tot_amount_due }}</label>
+                </th>
+              </tr>
+              <tr class="border-2 border-blue-500">
+                <th class="p-2 border-r border-blue-500">
+                  <label class="text-base text-black font-normal border-b-0">Amount Received</label>
+                </th>
+                <th class="p-2">
+                  <label class="text-base text-black font-normal">{{ i.amount_recived }} </label>
+                </th>
+              </tr>
+              <tr class="border-2 border-blue-500">
+                <th class="p-2 border-r border-blue-500">
+                  <label class="text-base text-black font-normal">Balance Due</label>
+                </th>
+                <th class="p-2">
+                  <label class="text-base text-black font-normal">{{ i.balance_due }} </label>
+                </th>
+              </tr>
+            </table>
+          </div>
+          <div class="sm:flex text-start ml-2 mt-4">
+            <label class="text-base w-3/4 sm:w-6/12 text-black font-normal">Payment Received in: {{ i.payment_received_in }}</label>
+            <input
+                class="border-0 border-b border-blue-400 w-6/12 sm:w-4/12 mr-2 sm:mr-0 ml-36 sm:ml-20 mt-2 sm:mt-0 text-center"
+                :value="i.received_by"
+            >
+          </div>
+          <div class="text-end mr-9 sm:mr-20">
+            <label class="text-base text-black font-normal">Received By</label>
+          </div>
+        </div>
+      </div>
+      <button @click="printReceipt" type="button"
+              class="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-md mt-3">
+        Download PDF
+      </button>
+      <button type="button" @click="updateFees" class="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-md mt-3">
+        Save
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,9 +90,8 @@ export default {
   methods: {
     async showReceipts() {
       let sr = this.$route.params.id;
-      console.log("Sr =", sr)
       let response = await axios.get(`Receipt/?sr=${sr}`)
-      console.log("Data =", response.data)
+      this.receipt_data = response.data
     }
   },
   mounted() {
